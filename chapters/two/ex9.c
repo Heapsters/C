e0500f64902033ee3b66a7e9999802e51a273d42
Richard Protasov <richardprotasov@gmail.com>
2015-11-19 00:55:48 -0500

    Adds solution for exercise 2-9

---
+++ chapters/two/ex9.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  ex9
+//
+//  Created by Richard Protasov on 11/18/15.
+//  Copyright © 2015 Richard Protasov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int bitcount(unsigned x);
+
+/* 
+ * Write a faster version of bit count
+ * with the understanding of why `x &= (x-1)`
+ * deletes the rightmost 1-bit of x in a two's
+ * complement system 
+*/
+int main(int argc, const char * argv[])
+{
+    bitcount(0xfff);
+    
+    return 0;
+}
+
+/* bitcount:  count 1 bits in x */
+int bitcount(unsigned x)
+{
+    int b = 0;
+    
+    while ((x &= (x-1)) != 0)
+        b++;
+    
+    return b;
+}
