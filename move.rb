32f6fc7bc5661a7d5262a07ebf6a7f425b239a2a
Richard Protasov <richardprotasov@gmail.com>
2015-12-12 13:16:28 -0500

    Includes a short description for usability

--- move.rb
+++ move.rb
@@ -1,8 +1,15 @@
 require "humanize"
 
-source = ARGV[0]
+#
+# Simple script to move solutions from its Xcode 
+# project strucure and parse
+# automatically generated header comments.
+#
+# ruby move.rb chapter:exercise
+#
 
-chapter, ex = source.split(":")
+chapter, ex = ARGV[0].split(":")
+
 chapter = Integer(chapter).humanize
 ex = "ex#{ex}"
 
